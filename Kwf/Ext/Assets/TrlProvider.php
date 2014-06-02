d653372dc4dd337a7acdc2688384e98f68756d45
Niko Sams <niko.sams@gmail.com>
2014-06-02 22:34:23 +0200

    move translation file from kwf

--- Kwf/Ext/Assets/TrlProvider.php
+++ Kwf/Ext/Assets/TrlProvider.php
@@ -6,17 +6,9 @@ class Kwf_Ext_Assets_TrlProvider extends Kwf_Assets_Provider_Abstract
         if ($dependency instanceof Kwf_Ext_Assets_JsDependency && $dependency->getFileNameWithType() == 'kwfext/src/Ext.js') {
             return array(
                 Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_USES => array(
-                    new Kwf_Assets_Dependency_File_Js('kwf/Kwf_js/Ext4/ext-lang-en.js')
+                    new Kwf_Assets_Dependency_File_Js('kwfext/ext-lang-en.js')
                 )
             );
-/*
-        } else if ($dependency instanceof Kwf_Assets_Ext4_Extensible_JsDependency && $dependency->getFileNameWithType() == 'extensible/src/Extensible.js') {
-            return array(
-                Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_USES => array(
-                    new Kwf_Assets_Dependency_File_Js('kwf/Kwf_js/Ext4/extensible-lang-en.js')
-                )
-            );
-*/
         }
         return array();
     }
