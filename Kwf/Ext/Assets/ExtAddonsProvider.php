3c73b29a5de7a8cab7778bdf8fd5a8d2fb30836b
Niko Sams <ns@vivid-planet.com>
2014-06-26 12:14:27 +0200

    remove code that now is in kwf-densajs

--- Kwf/Ext/Assets/ExtAddonsProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Kwf_Ext_Assets_ExtAddonsProvider extends Kwf_Assets_Provider_JsClass
-{
-    public function __construct()
-    {
-        parent::__construct(VENDOR_PATH.'/koala-framework/extjs-addons/Kwf/Ext4', 'kwfExtjsAddons/Kwf/Ext4', 'Kwf.Ext4');
-    }
-}
