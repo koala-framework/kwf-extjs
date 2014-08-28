39f69ad3918e8e2dfcac367ed08a56c84043be45
Niko Sams <ns@vivid-planet.com>
2014-08-28 09:43:42 +0200

    fix ext trl, load translation after class/Loader as it depends on Ext.onReady
    Ext.onReady is defined in Ext.class.Loader

--- Kwf/Ext/Assets/TrlProvider.php
+++ Kwf/Ext/Assets/TrlProvider.php
@@ -3,7 +3,7 @@ class Kwf_Ext_Assets_TrlProvider extends Kwf_Assets_Provider_Abstract
 {
     public function getDependenciesForDependency(Kwf_Assets_Dependency_Abstract $dependency)
     {
-        if ($dependency instanceof Kwf_Ext_Assets_JsDependency && $dependency->getFileNameWithType() == 'ext/src/Ext.js') {
+        if ($dependency instanceof Kwf_Ext_Assets_JsDependency && $dependency->getFileNameWithType() == 'ext/src/class/Loader.js') {
             return array(
                 Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_USES => array(
                     new Kwf_Assets_Dependency_File_Js('kwfext/ext-lang-en.js')
