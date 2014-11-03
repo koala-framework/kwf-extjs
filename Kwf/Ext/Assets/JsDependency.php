0f3e2995b1fbe189bfc55f8c6cc5bc0c25990950
Niko Sams <ns@vivid-planet.com>
2014-11-03 18:08:48 +0100

    disable Ext.Loader - it doesn't work anyway
    with that we get proper exceptions when trying to load something using Ext.Loader

--- Kwf/Ext/Assets/JsDependency.php
+++ Kwf/Ext/Assets/JsDependency.php
@@ -11,11 +11,7 @@ class Kwf_Ext_Assets_JsDependency extends Kwf_Assets_Dependency_File_Js
         } else if ($this->getFileNameWithType() == 'ext/src/class/Loader.js') {
             $ret .= "\n";
             $ret .= "Ext.Loader.setConfig({\n";
-            $ret .= "    enabled: true,\n";
-            $ret .= "    disableCaching: false,\n";
-            $ret .= "    paths: {\n";
-            $ret .= "        'Ext': '/assets/ext/src'\n";
-            $ret .= "    }\n";
+            $ret .= "    enabled: false\n";
             $ret .= "});\n";
         }
         $ret = "(function(Ext) {".$ret."})(this.Ext4 || (this.Ext4 = {}));";
