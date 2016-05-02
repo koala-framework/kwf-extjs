911c66d69500159c8fd6a7576a78a8fb31287c66
Niko Sams <ns@vivid-planet.com>
2016-05-02 17:24:41 +0200

    Paging toolbar config setting for default paging

--- js/grid/Panel.js
+++ js/grid/Panel.js
@@ -4,6 +4,7 @@ Ext.define('KwfExt.grid.Panel', {
     requires: [
         'KwfExt.grid.PanelController',
         'KwfExt.grid.PanelModel',
+        'Ext.toolbar.Paging',
         'KwfExt.mixin.Bindable' //fixes issues for storing selection in viewmodel
     ],
     controller: 'KwfExt.grid.Panel',
@@ -12,10 +13,46 @@ Ext.define('KwfExt.grid.Panel', {
     },
     stateful: true,
     config: {
-        editWindow: null
+        editWindow: null,
+        pagingToolbar: false
     },
     viewConfig: {
         preserveScrollOnRefresh: true
     },
-    publishes: ['selection', 'store']
+    publishes: ['selection', 'store'],
+
+    _getProperty: function(c)
+    {
+        var d = this[c];
+        if (d) {
+            d = Ext.clone(d)
+        } else {
+            d = []
+        }
+        return d
+    },
+
+    initComponent: function()
+    {
+        var dockedItems = this._getProperty("dockedItems");
+        if (this.getPagingToolbar()) {
+            dockedItems.push(this.getPagingToolbar());
+        }
+        if (dockedItems.length) {
+            this.dockedItems = dockedItems;
+        }
+
+        this.callParent(arguments);
+    },
+
+    applyPagingToolbar: function(b) {
+        if (b && !b.isComponent) {
+            b = Ext.create(Ext.apply({
+                xtype: "pagingtoolbar",
+                dock: "bottom",
+                displayInfo: true
+            }, b))
+        }
+        return b
+    }
 });
