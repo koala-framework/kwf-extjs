5ed238741993345ca4cf16eae0401e7c96fe1e5d
Niko Sams <ns@vivid-planet.com>
2016-05-11 08:52:45 +0200

    Fix default value bind
    apply fix from https://github.com/alexeysolonets/extjs-mvvm-extensions/issues/1
    
    Also remove store from published values in grid, as this breaks grid stores
    and isn't needed.

--- js/grid/Panel.js
+++ js/grid/Panel.js
@@ -31,7 +31,7 @@ Ext.define('KwfExt.grid.Panel', {
     viewConfig: {
         preserveScrollOnRefresh: true
     },
-    publishes: ['selection', 'store'],
+    publishes: ['selection'],
 
     _getProperty: function(c)
     {
--- js/grid/PanelModel.js
+++ js/grid/PanelModel.js
@@ -2,7 +2,6 @@ Ext.define('KwfExt.grid.PanelModel', {
     extend: 'Ext.app.ViewModel',
     alias: 'viewmodel.KwfExt.grid.Panel',
     data: {
-        selection: null,
-        store: null
+        selection: null
     }
 });
